f8ed9e2d0e8207b1cadb76794ae82fecd4a1aa8e
Jose <jbenite2@nd.edu>
2024-03-05 17:30:36 -0400

    WIP

--- client.cpp
+++ client.cpp
@@ -45,11 +45,6 @@ int main(int argc, char *argv[])
 	  std::string filename = getLastToken(argv[3], '/');
 
 
-	  //Print all arguments
-	  for (int i = 0; i < argc; i++) {
-		std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
-	  }
-
 	  //Make sure it's a valid port
 	  //Greater than 1023
 	  //Less than 65535
