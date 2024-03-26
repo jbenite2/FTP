c31d242c40d26ff96b79d41d3090a44e19c78c22
Jose <jbenite2@nd.edu>
2024-03-26 18:26:16 -0400

    Back

--- server.cpp
+++ server.cpp
@@ -1,5 +1,5 @@
+#include <iostream>
 #include <sys/types.h>
-#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,127 +7,131 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <sstream>
+#include <sys/stat.h>
+#include <vector>
+#include <signal.h>
 
+int terminateSignalReceived = 0;
+int connectionId = 1;
 
-void handleConnection(int clientSocketfd, const std::string& dir, int connectionID) {
-    // create output file path and open it
-    std::string filePath = dir + "/" + std::to_string(connectionID) + ".file";
-    std::ofstream outFile(filePath, std::ios::binary);
-    if (!outFile.is_open()) {
-        std::cerr << "ERROR: Could not open file for writing." << std::endl;
-        return;
+void signalHandler(int signal) {
+    if (signal == SIGQUIT || signal == SIGTERM) {
+        terminateSignalReceived = 1;
     }
-
-    char buffer[1024];
-    ssize_t bytesRead;
-
-    // set up timeout
-    struct timeval tv;
-    tv.tv_sec = 10; // 10 second timeout
-    tv.tv_usec = 0;
-    setsockopt(clientSocketfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
-
-    while (true) {
-        // read client file content
-        bytesRead = recv(clientSocketfd, buffer, sizeof(buffer), 0);
-        if (bytesRead < 0) {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                std::cerr << "ERROR: Timeout occurred. No data recieved." << std::endl;
-                outFile.seekp(0);
-                outFile.clear();
-                outFile << "ERROR";
-                break;
-            } else {
-                std::cerr << "ERROR: " << strerror(errno) << std::endl;
-                break;
-            }
-        } else if (bytesRead == 0) {
-            // client closed connection
-            break;
-        } else {
-            // write content to new file
-            outFile.write(buffer, bytesRead);
-        }
-    }
-
-    outFile.close();
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <PORT> <FILE-DIR>" << std::endl;
+int main(int argc, char *argv[]) {
+    // Server handles SIGTERM / SIGQUIT signals
+    signal(SIGQUIT, signalHandler);
+    signal(SIGTERM, signalHandler);
+
+    // Create a socket using TCP IP
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Allow others to reuse the address
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("setsockopt");
         return 1;
     }
 
-    int port = atoi(argv[1]);
-    if (port <= 1023 || port > 65535) {
-        std::cerr << "ERROR: Invalid port number" << std::endl;
-        return 2;
+    // Bind to a port and an address
+    if (argc != 3) {
+        std::cerr << "Error: incorrect number of arguments\n";
+        return 1;
     }
 
-    std::string fileDir = argv[2];
+    int port = std::stoi(argv[1]);
 
-    // create a socket using TCP IP
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        std::cerr << "ERROR: " << strerror(errno) << std::endl;
-        return 3;
-    }
-
-    // allow others to reuse the address
-    int yes = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        std::cerr << "ERROR: " << strerror(errno) << std::endl;
-        close(sockfd);
-        return 4;
-    }
-
-    // bind address to socket
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
     memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
 
+    if (port < 1024 || port > 65534) {
+        std::cerr << "ERROR: invalid port number\n";
+        return 1;
+    }
+
     if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        std::cerr << "ERROR: " << strerror(errno) << std::endl;
-        close(sockfd);
-        return 5;
+        perror("bind");
+        return 2;
     }
 
-    // set socket to listen status
     if (listen(sockfd, 1) == -1) {
-        std::cerr << "ERROR: " << strerror(errno) << std::endl;
-        close(sockfd);
-        return 6;
+        perror("listen");
+        return 3;
     }
 
-    // connection counter
-    int connectionID = 0;
-    while (true) {
-        // accept a new connection from a client
-        struct sockaddr_in clientAddr;
-        socklen_t clientAddrSize = sizeof(clientAddr);
-        int clientSocketfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
-        if (clientSocketfd == -1) {
-            if (errno == EINTR) {
-                break;
-            }
-            std::cerr << "ERROR: " << strerror(errno) << std::endl;
-            continue;
-        }
+    while (1) {
 
-        connectionID += 1;
-        handleConnection(clientSocketfd, fileDir, connectionID);
-        close(clientSocketfd);
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(sockfd, &readfds);
+   
+		if (FD_ISSET(sockfd, &readfds)) {
+            // Accept a new connection from a client
+            struct sockaddr_in clientAddr;
+            socklen_t clientAddrSize = sizeof(clientAddr);
+            int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
+
+            char ipstr[INET_ADDRSTRLEN] = {'\0'};
+            inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+            std::cout << "Accept a connection from: " << ipstr << ":" << ntohs(clientAddr.sin_port) << std::endl;
+
+            std::string directoryPath = std::string(argv[2]);
+            int result = mkdir(directoryPath.c_str(), 0777);
+            if (result == -1 && errno != EEXIST) {
+                std::cerr << "Error creating directory: " << strerror(errno) << std::endl;
+            }
+
+            std::string filePath = directoryPath + "/" + std::to_string(connectionId) + ".file";
+            connectionId++;
+
+            std::fstream outputFile;
+            outputFile.open(filePath, std::ios::out);
+
+            if (!outputFile.is_open()) {
+                std::cerr << "Error: cannot open the file (" << filePath << ")" << std::endl;
+                return 6;
+            }
+
+			// Initialize buffer to read data from the client
+            std::vector<char> buffer(1024);
+            ssize_t bytesRead;
+
+			// Set timeout for the socket
+			struct timeval timeout;
+			timeout.tv_sec = 10;
+			timeout.tv_usec = 0;
+			setsockopt(clientSockfd, SOL_SOCKET,SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+
+            while ((bytesRead = recv(clientSockfd, buffer.data(), buffer.size(), 0)) > 0) {
+                if (bytesRead < 0) {
+					if (errno == EAGAIN || errno == EWOULDBLOCK) {
+						std::cerr << "ERROR: Timeout occurred. " << std::endl; 
+						outputFile.seekp(0) ;
+						outputFile.clear();
+						outputFile<<"ERROR";
+						break;
+					}
+                } else if (bytesRead == 0) {
+					break;
+				} else {
+					outputFile.write(buffer.data(), bytesRead);
+				}
+            }
+
+			outputFile.close();
+            shutdown(clientSockfd, SHUT_RDWR);
+            close(clientSockfd);
+        }
     }
 
     close(sockfd);
+
     return 0;
 }
+
